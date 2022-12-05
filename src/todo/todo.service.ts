ccddf153091042f1459bc54b1107e120b4f8d34d
Gayath chandula <chandulagayan@gmail.com>
2022-12-05 07:34:13 +0530

    path change

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { Todo, TodoDocument } from './schemas/todo.schema';
+import { Todo, TodoDocument } from '../todo/Schemas/todo.schema';
 
 @Injectable()
 export class TodoService {
