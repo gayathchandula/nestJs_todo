eda2131f83c3532daaff28c34afc076b8a02d23c
Gayath chandula <chandulagayan@gmail.com>
2022-12-05 07:27:51 +0530

    changes

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Todo, TodoSchema } from './schemas/todo.schema';
+import { Todo, TodoSchema } from '../todo/Schemas/todo.schema';
 
 @Module({
   providers: [TodoService],
